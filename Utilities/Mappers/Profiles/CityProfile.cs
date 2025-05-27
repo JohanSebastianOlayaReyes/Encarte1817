dce7fe5a6e4df4249336e1f50b55666e08d6f5df
JohanSebastianOlayaReyes <jsolayareyes@gmail.com>
2025-05-27 16:29:54 -0500

    implementadno el AutoMappers

---
+++ Utilities/Mappers/Profiles/CityProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Entity.Dtos.CityDto;
+using Entity.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Mappers.Profiles
+{
+    public class CityProfile : Profile
+    {
+        public CityProfile()
+        {
+            CreateMap<City, CityDto>().ReverseMap();
+        }
+    }
+}
